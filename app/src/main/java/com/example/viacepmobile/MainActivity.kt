1cf7eb5cc85e1bb9b3c2f3653aeb11a3c6b06153
Geraldo Neto <netogeraldo209@gmail.com>
2024-09-06 16:24:38 -0300

    end

--- app/src/main/java/com/example/viacepmobile/MainActivity.kt
+++ app/src/main/java/com/example/viacepmobile/MainActivity.kt
@@ -22,16 +22,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Set the UI using Jetpack Compose
         setContent {
             ViacepmobileTheme {
-                // Scaffold layout to include top bars, fab, etc.
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> // innerPadding é passado aqui
-                    // Variáveis de estado para armazenar os dados
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     var usuarioList by remember { mutableStateOf<List<Usuario>>(emptyList()) }
                     var endereco by remember { mutableStateOf<Endereco?>(null) }
 
-                    // Chamar Firebase e Retrofit ao iniciar
                     LaunchedEffect(Unit) {
                         loadUsuarios { usuarios ->
                             usuarioList = usuarios
@@ -39,17 +36,14 @@ class MainActivity : ComponentActivity() {
                         endereco = buscarEndereco()
                     }
 
-                    // Adiciona o padding interno ao layout
                     Column(modifier = Modifier
                         .fillMaxSize()
-                        .padding(innerPadding)) { // Usando innerPadding no layout
+                        .padding(innerPadding)) {
 
-                        // Exibir dados do endereço se carregado
                         if (endereco != null) {
                             EnderecoInfo(endereco!!)
                         }
 
-                        // Exibir dados do primeiro usuário ou uma mensagem padrão
                         if (usuarioList.isNotEmpty()) {
                             Greeting(name = usuarioList[0].nome)
                         } else {
